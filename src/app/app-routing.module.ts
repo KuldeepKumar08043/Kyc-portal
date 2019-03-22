e94ccf5f3fdf68496916b55a4ee481a1f005c45b
Kuldeep Kumar <A1A5IZIX@INDIA.AIRTEL.ITM>
2019-03-22 15:30:54 +0530

    kyc-portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "/",
+    component: LoginComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "dashboard/:id",
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MatListModule } from '@angular/material/list';
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app.routing.module';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavBarComponent } from './sidenav-bar/sidenav-bar.component';
+import { SalesServiceService } from './sales-service.service';
+import { SlideshowModule } from 'ng-simple-slideshow';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidenavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,ReactiveFormsModule,
+    AppRoutingModule,
+    MatListModule, HttpClientModule, SlideshowModule
   ],
-  providers: [],
+  providers: [SalesServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
